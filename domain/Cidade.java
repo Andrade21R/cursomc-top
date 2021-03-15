764bd7fdbc3b85be722d41b79501011d2b9a57bd
Rafael Andrade dos Santos <rafael.santos.andrade@hotmail.com>
2021-03-15 13:58:45 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ domain/Cidade.java
@@ -0,0 +1,45 @@
+package com.neliosalves.cursomc.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cidade implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	@JsonManagedReference
+	@ManyToOne
+	@JoinColumn(name = "estado_id")
+	private Estado estado;
+
+	public Cidade(Integer id, String nome, Estado estado) {
+		this.id = id;
+		this.nome = nome;
+		this.estado = estado;
+	}
+
+}
